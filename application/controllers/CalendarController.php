d648b9091c77e48628195a6d7796ae3dbd00b916
dan <danielkram@gmail.com>
2010-06-08 16:06:16 -0700

    added css and finished calendar partial and main calendar section

--- application/controllers/CalendarController.php
+++ application/controllers/CalendarController.php
@@ -5,15 +5,20 @@ class CalendarController extends Zend_Controller_Action
 
     public function init()
     {
-    	$this->setLayout(false);
-        /* Initialize action controller here */
     }
-
+    
     public function indexAction()
     {
-        // action body
+    	
     }
 
-
+    public function getEventsAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+    	$calendar = new Model_DbTable_Calendar;
+    	$events = $calendar->getEvents($this->_getParam('start'), $this->_getParam('end'), $this->_getParam('userid'), $this->_getParam('type'));
+    	echo json_encode($events);
+    }
 }
 
